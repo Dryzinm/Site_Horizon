d07abdd8249f658368d9f61c92455e005e555231
Dryzinm <andrey.sousa3@estudante.ifto.edu.br>
2026-03-10 17:59:40 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById("btnMenu");
-const menu = document.getElementById("menu");
-
-botao.addEventListener("click", function () {
-    menu.classList.toggle("ativo");
-});
